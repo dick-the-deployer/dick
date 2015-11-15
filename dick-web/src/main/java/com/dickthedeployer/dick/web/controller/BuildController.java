de70c92fc0b58472c400e852d76366bc9f4f5d49
Your Name <you@example.com>
2015-11-15 21:04:04 +0100

    Fixed builds mapping

--- dick-web/src/main/java/com/dickthedeployer/dick/web/controller/BuildController.java
+++ dick-web/src/main/java/com/dickthedeployer/dick/web/controller/BuildController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author mariusz
  */
 @RestController
-@RequestMapping("/stacks")
+@RequestMapping("/builds")
 public class BuildController {
 
     @Autowired
