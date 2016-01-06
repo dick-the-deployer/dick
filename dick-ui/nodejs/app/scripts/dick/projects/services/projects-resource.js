e6d9aa062661b8b92dd68b3546ab896705930934
mariusz.luciow <mariusz.luciow@gmail.com>
2016-01-06 18:12:27 +0100

    Fixed filtering bug

--- dick-ui/nodejs/app/scripts/dick/projects/services/projects-resource.js
+++ dick-ui/nodejs/app/scripts/dick/projects/services/projects-resource.js
@@ -4,6 +4,7 @@ angular.module('dick.projects')
         .factory('ProjectsResource', ['$resource',
             function ($resource) {
                 return $resource('/api/projects/:namespace/:name', {}, {
+                    query: {method: 'GET', isArray: true, url: '/api/projects'},
                     allSilently: {method: 'GET', isArray: true, url: '/api/projects/all'},
                     builds: {method: 'GET', isArray: true, url: '/api/projects/:namespace/:name/builds'}
                 });
