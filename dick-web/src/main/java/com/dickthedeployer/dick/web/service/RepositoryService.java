a59e5dffa25919aa944d7b8bc8b7536d877992bf
mariusz.luciow <mariusz.luciow@gmail.com>
2016-02-21 19:45:36 +0100

    Added checking changes

--- dick-web/src/main/java/com/dickthedeployer/dick/web/service/RepositoryService.java
+++ dick-web/src/main/java/com/dickthedeployer/dick/web/service/RepositoryService.java
@@ -133,6 +133,7 @@ public class RepositoryService {
 
     private void checkoutRevision(Path path, String ref, String sha) {
         commandService.invoke(path, "git", "fetch", "origin");
+        commandService.invoke(path, "git", "pull", "origin", ref);
         commandService.invoke(path, "git", "checkout", ref);
         commandService.invoke(path, "git", "checkout", sha);
     }
