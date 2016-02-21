868022b3803801c7791800199052a36077c6d739
mariusz.luciow <mariusz.luciow@gmail.com>
2016-02-21 20:36:20 +0100

    Added cleaning

--- dick-web/src/main/java/com/dickthedeployer/dick/web/service/RepositoryService.java
+++ dick-web/src/main/java/com/dickthedeployer/dick/web/service/RepositoryService.java
@@ -132,7 +132,8 @@ public class RepositoryService {
     }
 
     private void checkoutRevision(Path path, String ref, String sha) {
-        commandService.invoke(path, "git", "fetch", "origin");
+        commandService.invoke(path, "git", "clean", "-fdx");
+        commandService.invoke(path, "git", "reset", "-q", "--hard");
         commandService.invoke(path, "git", "pull", "origin", ref);
         commandService.invoke(path, "git", "checkout", ref);
         commandService.invoke(path, "git", "checkout", sha);
