b9708d7bd706c021f5c7b126e0e46908948e31c1
Mariusz Luciow <mariusz.luciow@ocado.com>
2016-06-04 12:38:38 +0200

    Working sso

--- dick-web/src/main/java/com/dickthedeployer/dick/autoconfiguration/SsoSecurityAutoConfiguration.java
+++ dick-web/src/main/java/com/dickthedeployer/dick/autoconfiguration/SsoSecurityAutoConfiguration.java
@@ -15,18 +15,40 @@
  */
 package com.dickthedeployer.dick.autoconfiguration;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.OAuth2AutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.client.OAuth2SsoProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerTokenServicesConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.OAuth2ClientConfiguration;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestHeaderRequestMatcher;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.Filter;
@@ -36,34 +58,96 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 
 @Configuration
+@AutoConfigureBefore({OAuth2AutoConfiguration.class})
 @ConditionalOnProperty(prefix = "security", name = "schema", havingValue = "oauth2")
 public class SsoSecurityAutoConfiguration {
 
 
-    @Controller
-    public static class LoginErrors {
+    @Configuration
+    @EnableConfigurationProperties(OAuth2SsoProperties.class)
+    @Import({OAuth2ClientConfiguration.class, ResourceServerTokenServicesConfiguration.class})
+    public static class SsoWebSecurityAdapter extends WebSecurityConfigurerAdapter implements BeanFactoryAware {
 
-        @RequestMapping("/dashboard/login")
-        public String dashboard() {
-            return "redirect:/#/";
-        }
-
-    }
-
-    @Component
-    @EnableOAuth2Sso
-    public static class LoginConfigurer extends WebSecurityConfigurerAdapter {
+        private BeanFactory beanFactory;
 
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            http.antMatcher("/dashboard/**").authorizeRequests().anyRequest()
+            http.requestMatchers().anyRequest().and()
+                    .authorizeRequests().anyRequest()
                     .authenticated().and().csrf()
                     .csrfTokenRepository(csrfTokenRepository()).and()
-                    .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class)
-                    .logout().logoutUrl("/dashboard/logout").permitAll()
-                    .logoutSuccessUrl("/");
+                    .addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+
+            OAuth2SsoProperties sso = this.beanFactory.getBean(OAuth2SsoProperties.class);
+            // Delay the processing of the filter until we know the
+            // SessionAuthenticationStrategy is available:
+            http.apply(
+                    (SecurityConfigurerAdapter) new OAuth2ClientAuthenticationConfigurer(oauth2SsoFilter(sso))
+            );
+            addAuthenticationEntryPoint(http, sso);
+        }
+
+        private OAuth2ClientAuthenticationProcessingFilter oauth2SsoFilter(
+                OAuth2SsoProperties sso) {
+            OAuth2RestOperations restTemplate = this.beanFactory
+                    .getBean(OAuth2RestOperations.class);
+            ResourceServerTokenServices tokenServices = this.beanFactory
+                    .getBean(ResourceServerTokenServices.class);
+            OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(
+                    sso.getLoginPath());
+            filter.setRestTemplate(restTemplate);
+            filter.setTokenServices(tokenServices);
+            return filter;
+        }
+
+        private void addAuthenticationEntryPoint(HttpSecurity http, OAuth2SsoProperties sso)
+                throws Exception {
+            ExceptionHandlingConfigurer<HttpSecurity> exceptions = http.exceptionHandling();
+            ContentNegotiationStrategy contentNegotiationStrategy = http
+                    .getSharedObject(ContentNegotiationStrategy.class);
+            if (contentNegotiationStrategy == null) {
+                contentNegotiationStrategy = new HeaderContentNegotiationStrategy();
+            }
+            MediaTypeRequestMatcher preferredMatcher = new MediaTypeRequestMatcher(
+                    contentNegotiationStrategy, MediaType.APPLICATION_XHTML_XML,
+                    new MediaType("image", "*"), MediaType.TEXT_HTML, MediaType.TEXT_PLAIN);
+            preferredMatcher.setIgnoredMediaTypes(Collections.singleton(MediaType.ALL));
+            exceptions.defaultAuthenticationEntryPointFor(
+                    new LoginUrlAuthenticationEntryPoint(sso.getLoginPath()),
+                    preferredMatcher);
+            // When multiple entry points are provided the default is the first one
+            exceptions.defaultAuthenticationEntryPointFor(
+                    new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
+                    new RequestHeaderRequestMatcher("X-Requested-With", "XMLHttpRequest"));
+        }
+
+        private static class OAuth2ClientAuthenticationConfigurer
+                extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+            private OAuth2ClientAuthenticationProcessingFilter filter;
+
+            OAuth2ClientAuthenticationConfigurer(
+                    OAuth2ClientAuthenticationProcessingFilter filter) {
+                this.filter = filter;
+            }
+
+            @Override
+            public void configure(HttpSecurity builder) throws Exception {
+                OAuth2ClientAuthenticationProcessingFilter ssoFilter = this.filter;
+                ssoFilter.setSessionAuthenticationStrategy(
+                        builder.getSharedObject(SessionAuthenticationStrategy.class));
+                builder.addFilterAfter(ssoFilter,
+                        AbstractPreAuthenticatedProcessingFilter.class);
+            }
+
+        }
+
+        @Override
+        public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+            this.beanFactory = beanFactory;
         }
 
         private Filter csrfHeaderFilter() {
